514ae2fcf3e73cf5c87db9a1b10c098427888afb
Funix565 <denkin.dmitriy@gmail.com>
2022-10-13 23:26:34 +0300

    Playing around with Basic UI components

--- app/src/main/java/lab1/progmob/whowantstobeamillionaire/MainActivity.kt
+++ app/src/main/java/lab1/progmob/whowantstobeamillionaire/MainActivity.kt
@@ -1,11 +1,39 @@
 package lab1.progmob.whowantstobeamillionaire
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import lab1.progmob.whowantstobeamillionaire.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // We use View Binding
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startGame.setOnClickListener {
+            onStartGamePressed()
+        }
+
+        binding.gameSettings.setOnClickListener {
+            onGameSettingsPressed()
+        }
     }
+
+    private fun onStartGamePressed() {
+        // TODO: Show questions
+        binding.startGame.setBackgroundColor(Color.CYAN)
+        binding.startGame.setText("New question")
+        binding.startGame.setTextColor(Color.BLACK)
+    }
+
+    private fun onGameSettingsPressed() {
+        // TODO: Go to settings
+    }
+
+
 }
